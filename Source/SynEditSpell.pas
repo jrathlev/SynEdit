f64491bad50d7c90cec42e721e456c3ddf5c4c2c
Jürgen Rathlev <info@rathlev-home.de>
2026-03-31 12:57:08 +0200

    Update SynEditSpell.pas

--- Source/SynEditSpell.pas
+++ Source/SynEditSpell.pas
@@ -23,7 +23,9 @@
    Vers. 1.2 - June 2022: Fixed issue on compiling as 64 bit application
    Vers. 1.3 - March 2024: Added "RemoveDictWord"  to remove an entry from the
                dictionary (suggested by Dimon-II - https://github.com/Dimon-II/DeCard64)
-   last modified: March 2024
+   Vers. 1.4 - March 2026: New functions for LanguageId processing
+
+   last modified: March 2026
    *)
 
 unit SynEditSpell;
@@ -36,12 +38,12 @@ uses Winapi.Windows, System.SysUtils, System.Classes, System.Contnrs,
 
 type
   TLanguage = record
-    Id : word;
+    Id : TLocaleID;
     Shortname : string;
     end;
 
   TLoadedDict = record
-    Id : word;
+    Id : TLocaleID;
     Filename,Shortname : string;
     mAff,mDic : TMemoryStream;
     end;
@@ -50,7 +52,7 @@ type
   TLoadedDicts = array of TLoadedDict;
 
   TDictionaryProps = class(TObject)
-    Id : word;
+    Id : TLocaleID;
     Shortname : string;
     constructor Create (AId : word; const sn : string);
     end;
@@ -99,7 +101,7 @@ type
   private
     FHunspellHandle: Pointer;
     FEnabled : boolean;
-    FLangId : word;
+    FLangId : TLocaleID;
     FBusy, FModified, FUseUserDictionary : Boolean;
     FDictPath, FUserFileName, FUserDictPath : String;
     FUnderlineColor : TColor;
@@ -125,13 +127,13 @@ type
     constructor Create (AOwner : TComponent; ACheckAttri : TCheckAttributes = AllAttributes);
     destructor Destroy; override;
 
-    function GetLanguageIndex (const AShortName : string) : integer;
-    function GetLanguageNameFromId (LangId : word) : string;
-    function GetDictIndex (LangId : word) : integer;
+//    function GetLanguageIndex (const AShortName : string) : integer;
+//    function GetLanguageNameFromId (LangId : TLocaleID) : string;
+    function GetDictIndex (LangId : TLocaleID) : integer;
     function GetDictionaryFiles : boolean;
     function LoadDictionaries (const APath : string; Dicts : TIdArray = nil) : boolean;
     function SelectDictionary (const LangShortname : String) : boolean; overload;
-    function SelectDictionary (LangId : word) : boolean; overload;
+    function SelectDictionary (LangId : TLocaleID) : boolean; overload;
     procedure CloseDictionary;
     procedure SaveUserDictionary;
     function GetDictLanguages (ALangList : TStringList) : boolean;
@@ -169,8 +171,8 @@ type
     property OnStart: TNotifyEvent read FOnStart write FOnStart;
   end;
 
-function LangIDToLangShortname(ALangID : Word) : string;
-function GetLanguageName(LangID: word): String;
+function GetLanguageShortName(LangID : TLocaleID) : string;
+function GetLanguageDisplayName(LangId : TLocaleID) : string;
 
 procedure Register;
 
@@ -182,155 +184,40 @@ const
   extAff = '.aff';
   extDic = '.dic';
 
-  Languages : array[0..108] of TLanguage = (
-    (Id: LANG_AFRIKAANS or SUBLANG_DEFAULT shl 10; Shortname: 'af'),
-    (Id: LANG_ALBANIAN or SUBLANG_DEFAULT shl 10; Shortname: 'sq'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_SAUDI_ARABIA shl 10; Shortname: 'ar-sa'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_IRAQ shl 10; Shortname: 'ar-iq'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_EGYPT shl 10; Shortname: 'ar-eg'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_LIBYA shl 10; Shortname: 'ar-ly'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_ALGERIA shl 10; Shortname: 'ar-dz'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_MOROCCO shl 10; Shortname: 'ar-ma'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_TUNISIA shl 10; Shortname: 'ar-tn'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_OMAN shl 10; Shortname: 'ar-om'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_YEMEN shl 10; Shortname: 'ar-ye'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_SYRIA shl 10; Shortname: 'ar-sy'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_JORDAN shl 10; Shortname: 'ar-jo'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_LEBANON shl 10; Shortname: 'ar-lb'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_KUWAIT shl 10; Shortname: 'ar-kw'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_UAE shl 10; Shortname: 'ar-ae'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_BAHRAIN shl 10; Shortname: 'ar-bh'),
-    (Id: LANG_ARABIC or SUBLANG_ARABIC_QATAR shl 10; Shortname: 'ar-qa'),
-    (Id: LANG_BASQUE or SUBLANG_DEFAULT shl 10; Shortname: 'eu'),
-    (Id: LANG_BELARUSIAN; Shortname: 'be'),
-    (Id: LANG_BULGARIAN; Shortname: 'bg'),
-    (Id: LANG_CATALAN; Shortname: 'ca'),
-    (Id: LANG_CHINESE; Shortname: 'zh'),
-    (Id: LANG_CHINESE or SUBLANG_CHINESE_TRADITIONAL shl 10; Shortname: 'zh-tw'),
-    (Id: LANG_CHINESE or SUBLANG_CHINESE_SIMPLIFIED shl 10; Shortname: 'zh-cn'),
-    (Id: LANG_CHINESE or SUBLANG_CHINESE_HONGKONG shl 10; Shortname: 'zh-hk'),
-    (Id: LANG_CHINESE or SUBLANG_CHINESE_SINGAPORE shl 10; Shortname: 'zh-sg'),
-    (Id: LANG_CROATIAN; Shortname: 'hr'),
-    (Id: LANG_CZECH; Shortname: 'cs'),
-    (Id: LANG_DANISH; Shortname: 'da'),
-    (Id: LANG_DUTCH or SUBLANG_DUTCH shl 10; Shortname: 'nl-nl'),
-    (Id: LANG_DUTCH or SUBLANG_DUTCH_BELGIAN shl 10; Shortname: 'nl-be'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_US shl 10; Shortname: 'en-us'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_UK shl 10; Shortname: 'en-uk'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_UK shl 10; Shortname: 'en-gb'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_AUS shl 10; Shortname: 'en-au'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_CAN shl 10; Shortname: 'en-ca'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_NZ shl 10; Shortname: 'en-nz'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_EIRE shl 10; Shortname: 'en-ie'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_SOUTH_AFRICA shl 10; Shortname: 'en-za'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_JAMAICA shl 10; Shortname: 'en-jm'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_CARIBBEAN shl 10; Shortname: 'en-cb'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_BELIZE shl 10; Shortname: 'en-bz'),
-    (Id: LANG_ENGLISH or SUBLANG_ENGLISH_TRINIDAD shl 10; Shortname: 'en-tt'),
-    (Id: LANG_ESTONIAN; Shortname: 'et'),
-    (Id: LANG_FAEROESE; Shortname: 'fo'),
-    (Id: LANG_FARSI; Shortname: 'fa'),
-    (Id: LANG_FINNISH; Shortname: 'fi'),
-    (Id: LANG_FRENCH or SUBLANG_FRENCH shl 10; Shortname: 'fr-fr'),
-    (Id: LANG_FRENCH or SUBLANG_FRENCH_BELGIAN shl 10; Shortname: 'fr-be'),
-    (Id: LANG_FRENCH or SUBLANG_FRENCH_CANADIAN shl 10; Shortname: 'fr-ca'),
-    (Id: LANG_FRENCH or SUBLANG_FRENCH_SWISS shl 10; Shortname: 'fr-ch'),
-    (Id: LANG_FRENCH or SUBLANG_FRENCH_LUXEMBOURG shl 10; Shortname: 'fr-lu'),
-    (Id: LANG_GERMAN or SUBLANG_GERMAN shl 10; Shortname: 'de-de'),
-    (Id: LANG_GERMAN or SUBLANG_GERMAN_SWISS shl 10; Shortname: 'de-ch'),
-    (Id: LANG_GERMAN or SUBLANG_GERMAN_AUSTRIAN shl 10; Shortname: 'de-at'),
-    (Id: LANG_GERMAN or SUBLANG_GERMAN_LUXEMBOURG shl 10; Shortname: 'de-lu'),
-    (Id: LANG_GERMAN or SUBLANG_GERMAN_LIECHTENSTEIN shl 10; Shortname: 'de-li'),
-    (Id: LANG_GREEK; Shortname: 'el'),
-    (Id: LANG_HEBREW; Shortname: 'he'),
-    (Id: LANG_HUNGARIAN; Shortname: 'hu'),
-    (Id: LANG_ICELANDIC; Shortname: 'is'),
-    (Id: LANG_INDONESIAN; Shortname: 'id'),
-    (Id: LANG_ITALIAN or SUBLANG_ITALIAN shl 10; Shortname: 'it-it'),
-    (Id: LANG_ITALIAN or SUBLANG_ITALIAN_SWISS shl 10; Shortname: 'it-ch'),
-    (Id: LANG_JAPANESE; Shortname: 'ja'),
-    (Id: LANG_KOREAN or SUBLANG_KOREAN shl 10; Shortname: 'ko'),
-    (Id: LANG_KOREAN or SUBLANG_KOREAN shl 10; Shortname: 'ko-ko'),
-    (Id: LANG_KOREAN or SUBLANG_KOREAN_JOHAB shl 10; Shortname: 'ko-kp'),
-    (Id: LANG_LATVIAN or SUBLANG_DEFAULT shl 10; Shortname: 'lv'),
-    (Id: LANG_LITHUANIAN or SUBLANG_DEFAULT shl 10; Shortname: 'lt'),
-    (Id: LANG_NORWEGIAN or SUBLANG_NORWEGIAN_BOKMAL shl 10; Shortname: 'no'),
-    (Id: LANG_NORWEGIAN or SUBLANG_NORWEGIAN_NYNORSK shl 10; Shortname: 'no-no'),
-    (Id: LANG_POLISH or SUBLANG_DEFAULT shl 10; Shortname: 'pl'),
-    (Id: LANG_PORTUGUESE or SUBLANG_PORTUGUESE shl 10; Shortname: 'pt-pt'),
-    (Id: LANG_PORTUGUESE or SUBLANG_PORTUGUESE_BRAZILIAN shl 1; Shortname: 'pt-br'),
-    (Id: LANG_ROMANIAN or SUBLANG_DEFAULT shl 10; Shortname: 'ro'),
-    (Id: LANG_RUSSIAN or SUBLANG_DEFAULT shl 10; Shortname: 'ru'),
-    (Id: LANG_SERBIAN or SUBLANG_DEFAULT shl 10; Shortname: 'sr'),
-    (Id: LANG_SERBIAN or SUBLANG_SERBIAN_LATIN shl 10; Shortname: 'sr'),
-    (Id: LANG_SERBIAN or SUBLANG_SERBIAN_CYRILLIC shl 10; Shortname: 'sr'),
-    (Id: LANG_SLOVAK or SUBLANG_DEFAULT shl 10; Shortname: 'sk'),
-    (Id: LANG_SLOVENIAN or SUBLANG_DEFAULT shl 10; Shortname: 'sl'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH shl 10; Shortname: 'es'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_MEXICAN shl 10; Shortname: 'es-mx'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_MODERN shl 10; Shortname: 'es'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_GUATEMALA shl 10; Shortname: 'es-gt'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_COSTA_RICA shl 10; Shortname: 'es-cr'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_PANAMA shl 10; Shortname: 'es-pa'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_DOMINICAN_REPUBLIC; Shortname: 'es-do'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_VENEZUELA shl 10; Shortname: 'es-ve'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_COLOMBIA shl 10; Shortname: 'es-co'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_PERU shl 10; Shortname: 'es-pe'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_ARGENTINA shl 10; Shortname: 'es-ar'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_ECUADOR shl 10; Shortname: 'es-ec'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_CHILE shl 10; Shortname: 'es-cl'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_URUGUAY shl 10; Shortname: 'es-uy'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_PARAGUAY shl 10; Shortname: 'es-py'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_BOLIVIA shl 10; Shortname: 'es-bo'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_EL_SALVADOR shl 10; Shortname: 'es-sv'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_HONDURAS shl 10; Shortname: 'es-hn'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_NICARAGUA shl 10; Shortname: 'es-ni'),
-    (Id: LANG_SPANISH or SUBLANG_SPANISH_PUERTO_RICO shl 10; Shortname: 'es-pr'),
-    (Id: LANG_SWEDISH or SUBLANG_SWEDISH shl 10; Shortname: 'sv'),
-    (Id: LANG_SWEDISH or SUBLANG_SWEDISH_FINLAND shl 10; Shortname: 'sv-fi'),
-    (Id: LANG_THAI or SUBLANG_DEFAULT shl 10; Shortname: 'th'),
-    (Id: LANG_TURKISH or SUBLANG_DEFAULT shl 10; Shortname: 'tr'),
-    (Id: LANG_UKRAINIAN or SUBLANG_DEFAULT shl 10; Shortname: 'uk'),
-    (Id: LANG_VIETNAMESE or SUBLANG_DEFAULT shl 10; Shortname: 'vi'));
-
-function LangIDToLangShortname(ALangID : Word) : string;
+function GetLanguageID (const LangName : string) : TLocaleID;
 var
-  i: Integer;
+  nc : cardinal;
 begin
-  for i:=Low(Languages) to High(Languages) do with Languages[i] do
-    if ALangID = Id then begin
-      Result:=Shortname;
-      Exit;
+  Result:=0; nc:=0;
+  if GetLocaleInfoEx(pchar(LangName),LOCALE_RETURN_NUMBER or LOCALE_ILANGUAGE,@nc,4)>0 then begin
+    Result:=nc;
     end;
-  Result:='';
   end;
 
-function LangShortnameToLangID(ALangName : String) : Word;
+function GetLanguageName (LangId : TLocaleID; Short : boolean) : string;
 var
-  i: Integer;
+  nc,nt : cardinal;
+  buf : array of Char;
 begin
-  for i:=Low(Languages) to High(Languages) do with Languages[i] do
-    if AnsiSameText(Shortname,ALangName) then begin
-      Result:=Id;
-      Exit;
+  Result:=''; nc:=0;
+  if Short then nt:=LOCALE_SNAME else nt:=LOCALE_SLOCALIZEDDISPLAYNAME;
+  nc:=GetLocaleInfo(LangId,nt,nil,nc);
+  if nc>0 then begin
+    SetLength(buf,nc);
+    if GetLocaleInfo(LangId,nt,@buf[0],nc)>0 then
+      Result:=PChar(@buf[0]);
+    buf:=nil;
     end;
-  // second try, primary language code only
-  ALangName:=copy(ALangName,1,2);
-  for i:=Low(Languages) to High(Languages) do with Languages[i] do
-    if AnsiSameText(ALangName,Copy(Shortname,1,2)) then begin
-      Result:=Id;
-      Exit;
-    end;
-  Result:=LANG_ENGLISH or SUBLANG_ENGLISH_US shl 10;
   end;
 
-function GetLanguageName(LangID: word): String;
-var
-  Buf: array [0..LOCALE_NAME_MAX_LENGTH-1] of Char;
+function GetLanguageShortName (LangId : TLocaleID) : string;
 begin
-  VerLanguageName(LangID,@Buf,LOCALE_NAME_MAX_LENGTH-1);
-  Result:=Buf;
-  if length(Result)=0 then Result:='<?>';
+  Result:=GetLanguageName(LangId,true);
+  end;
+
+function GetLanguageDisplayName (LangId : TLocaleID) : string;
+begin
+  Result:=GetLanguageName(LangId,false);
   end;
 
 procedure Register;
@@ -507,28 +394,7 @@ begin
   FUnderlineStyle:=Value;
   end;
 
-function TSynEditSpellCheck.GetLanguageNameFromId (LangId : word) : string;
-begin
-
-  end;
-
-// Get index from language list matching to AShortName
-function TSynEditSpellCheck.GetLanguageIndex (const AShortName : string) : integer;
-var
-  sl : string;
-begin
-  sl:=copy(AShortName,1,5);
-  for Result:=0 to High(Languages) do begin
-    if AnsiSameText(sl,Languages[Result].ShortName) then Exit
-    else if AnsiSameText(AnsiReplaceStr(sl,'_','-'),Languages[Result].ShortName) then Exit;
-    end;
-  sl:=copy(AShortName,1,2);
-  for Result:=0 to High(Languages) do
-    if AnsiSameText(sl,Languages[Result].ShortName) then Exit;
-  Result:=-1;
-  end;
-
-function TSynEditSpellCheck.GetDictIndex (LangId : word) : integer;
+function TSynEditSpellCheck.GetDictIndex (LangId : TLocaleID) : integer;
 begin
   for Result:=0 to High(FLoadedDicts) do
     if LangId=FLoadedDicts[Result].Id then Exit;
@@ -547,11 +413,13 @@ begin
     FindResult:=FindFirst(FDictPath+'*'+extAff,faAnyFile,SearchRec);
     while (FindResult=0) do begin
       sn:=ChangeFileExt(SearchRec.Name,'');
-      n:=GetLanguageIndex(sn);
-      if (n>=0) then begin
-        with Languages[n] do
-          FDictionaries.AddObject(sn,TDictionaryProps.Create(Id,Shortname));
-        end;
+      FDictionaries.AddObject(sn,TDictionaryProps.Create(GetLanguageID(sn),sn));
+
+//      n:=GetLanguageIndex(sn);
+//      if (n>=0) then begin
+//        with Languages[n] do
+//          FDictionaries.AddObject(sn,TDictionaryProps.Create(Id,Shortname));
+//        end;
       FindResult:=FindNext(SearchRec);
       end;
     FindClose(SearchRec);
@@ -564,7 +432,7 @@ function TSynEditSpellCheck.LoadDictionaries (const APath : string; Dicts : TIdA
 var
   i,j,dcnt : integer;
   sn : string;
-  aid    : word;
+  aid    : TLocaleID;
   ok     : boolean;
 begin
   Result:=false;
@@ -618,11 +486,11 @@ begin
 
 function TSynEditSpellCheck.GetDictLanguageName : string;
 begin
-  Result:=GetLanguageName(FLangId);
+  Result:=GetLanguageDisplayName(FLangId);
   end;
 
 // Select the current dictionary associated to LangId
-function TSynEditSpellCheck.SelectDictionary (LangId : word) : boolean;
+function TSynEditSpellCheck.SelectDictionary (LangId : TLocaleID) : boolean;
 var
   n     : integer;
   sn,sl : string;
@@ -682,7 +550,7 @@ begin
 
 function TSynEditSpellCheck.SelectDictionary (const LangShortname : String) : boolean;
 begin
-  Result:=SelectDictionary(LangShortnameToLangID(LangShortname));
+  Result:=SelectDictionary(GetLanguageID(LangShortname));
   end;
 
 procedure TSynEditSpellCheck.UserDictChange (Sender : TObject);
@@ -713,11 +581,11 @@ begin
 function TSynEditSpellCheck.GetDictLanguages (ALangList : TStringList) : boolean;
 var
   i : integer;
-  w : word;
+  w : TLocaleID;
 begin
   for i:=0 to High(FLoadedDicts) do begin
     w:=FLoadedDicts[i].Id;
-    ALangList.AddObject(GetLanguageName(w),pointer(w));
+    ALangList.AddObject(GetLanguageDisplayName(w),pointer(w));
     end;
   Result:=ALangList.Count>0;
   end;
